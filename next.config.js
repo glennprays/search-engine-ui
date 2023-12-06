1fa1e67740e7a93572708b5832689601e164f410
glennprays <88752553+glennprays@users.noreply.github.com>
2023-12-07 00:53:00 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-// module.exports = nextConfig
+module.exports = nextConfig
 
-module.exports = {
-    async rewrites() {
-      return [
-        {
-          source: '/:path*',
-          destination: 'http://localhost:5000/:path*' // Proxy to Backend
-        }
-      ]
-    }
-  }
